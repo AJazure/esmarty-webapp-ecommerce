8da78fc0ef62101e2c8bfd461c33472cfcb15ceb
CristianMene <143648242+CristianMene@users.noreply.github.com>
2023-10-30 02:30:11 -0300

    Verificacion al registrarse
    ir a mailtrap.io, registrarse, vas a EmailTesting, entras en el que esta, en integrations elegis Laravel 9+, copias lo de abajo y los pegas en el archivo .env

--- routes/panel.php
+++ routes/panel.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 	return view('panel.index');
-});
+})->middleware(['verified']);
 Route::resource('/proveedores', ProveedorController::class)->names('proveedor'); 
 Route::resource('/productos', ProductoController::class)->names('producto');
 Route::resource('/marcas', MarcaController::class)->names('marca'); //como es un controlador tipo resource usaré solo esta línea
